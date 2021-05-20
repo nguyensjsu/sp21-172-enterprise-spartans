3a940d374943b8822f4a89499bcd78c2a1ce5b41
miguelg719 <miguelg71921@gmail.com>
2021-05-19 18:51:28 -0700

    connected online-store and backend

--- online-store/src/App.js
+++ online-store/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useEffect } from "react";
+import React, { useEffect, Component } from "react";
 import Home from './Home/Home.js';
 import Header from './Home/Header/Header.js'
 import MenuScreen from './Menu/MenuScreen.js';
@@ -14,35 +14,61 @@ import {
   Redirect,
 } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <Header></Header>
-            <Home></Home>
-            <Footer></Footer>
-          </Route>
-          <Route exact path="/menu">
-            <Header></Header>
-            <MenuScreen />
-            <Footer></Footer>
-          </Route>
-          <Route exact path="/gifts">
-            <Header></Header>
-            <GiftScreen />
-            <Footer></Footer>
-          </Route>
-          <Route exact path="/rewards">
-            <Header></Header>
-            <RewardsScreen />
-            <Footer></Footer>
-          </Route>
-        </Switch>
-      </Router>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    isLoading: true,
+    cards: []
+  };
+
+  async componentDidMount() {
+    const response = await fetch('/api/cards');
+    const body = await response.json();
+    this.setState({ cards: body, isLoading: false });
+  }
+
+  render() {
+
+    const { cards, isLoading } = this.state;
+
+    if (isLoading) {
+      return <p>Loading...</p>;
+    }
+
+    return (
+      <div className="App">
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <Header></Header>
+              {cards.map(card =>
+                <div key={card.id}>
+                  {card.cardNumber}
+                </div>
+              )}
+              <Home></Home>
+              <Footer></Footer>
+            </Route>
+            <Route exact path="/menu">
+              <Header></Header>
+              <MenuScreen />
+              <Footer></Footer>
+            </Route>
+            <Route exact path="/gifts">
+              <Header></Header>
+              <GiftScreen />
+              <Footer></Footer>
+            </Route>
+            <Route exact path="/rewards">
+              <Header></Header>
+              <RewardsScreen />
+              <Footer></Footer>
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
